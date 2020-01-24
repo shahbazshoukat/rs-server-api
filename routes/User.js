25a34e7ef3e5922c4684780ceda01fabcf952fb0
shahbaz shoukat <shahbazshoukat09@gmail.com>
2020-01-24 22:09:46 +0500

    fixes

--- routes/User.js
+++ routes/User.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Auth = require('../middleware/Auth');
 
 const router = express.Router();
 const UsersController = require('../app/user/UserCtrl');
@@ -7,6 +8,6 @@ router.post('/signUp', UsersController.createNewUser);
 
 router.post('/login', UsersController.loginUser);
 
-router.post('/logout', UsersController.logoutUser);
+router.post('/logout', Auth.Authenticate, UsersController.logoutUser);
 
 module.exports = router;
