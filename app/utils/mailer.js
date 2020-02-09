38f070894e529115e49a311107681d39ef725430
shahbaz shoukat <shahbazshoukat09@gmail.com>
2020-02-09 17:32:17 +0500

    email transport sendgrid setup

--- app/utils/mailer.js
+++ app/utils/mailer.js
@@ -4,70 +4,31 @@ const {
   config
 } = require('../../helpers');
 
+const defaultTransport = config.defaultMailTransport || 'gmail';
+const selectedTransport = config.mail.transports.get(defaultTransport);
+
+const transport = nodemailer.createTransport(selectedTransport);
+
 exports.sendCommentEmail = (to, link) => {
 
-  const transporter = nodemailer.createTransport({
-    service: config.email.service,
-    auth: {
-      user: config.email.auth.user,
-      pass: config.email.auth.pass
-    }
-  });
-
   const mailOptions = {
-    from: config.email.defaultEmail,
+    from: config.mail.addresses.account,
     to: to.email,
     subject: 'ResultSquare | New Comment',
     html:
         `<h1>ResultSquare | New Comment</h1><p>New Comment added, click </p><a href=${link}>Here</a><p> to view.</p>`
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-
-    if (error) {
-
-      console.log(error);
-
-    } else {
-
-      console.log(`Email sent: ${info.response}`);
-
-    }
-
-  });
+  return sendEmail(mailOptions);
 
 };
 
-exports.sendEmail = (email, token) => {
+const sendEmail = options => new Promise((resolve, reject) => {
 
-  const link = `http://localhost:4200/resetpassword/${token}`;
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'shahbaz.shoukat.330@gmail.com',
-      pass: 'drowssaP@009'
-    }
-  });
-  const mailOptions = {
-    from: 'shahbaz.shoukat.330@gmail.com',
-    to: email,
-    subject: 'Todoos | Reset Password',
-    html:
-      `<h1>ToDoos | Reset Password</h1><p>Please Click </p><a href=${link}>Here</a><p> to reset your password.</p>`
-  };
+  transport.sendMail(options, (error, info) => {
 
-  transporter.sendMail(mailOptions, (error, info) => {
-
-    if (error) {
-
-      console.log(error);
-
-    } else {
-
-      console.log(`Email sent: ${info.response}`);
-
-    }
+    (error && reject(error)) || resolve(info);
 
   });
 
-};
+});
