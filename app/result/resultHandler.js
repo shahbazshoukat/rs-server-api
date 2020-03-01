c283dc68462edee931c32d7ba17f69e57dc62f12
shahbaz shoukat <shahbazshoukat09@gmail.com>
2020-03-01 22:52:51 +0500

    updates

--- app/result/resultHandler.js
+++ app/result/resultHandler.js
@@ -121,11 +121,6 @@ class ResultHandler {
         $project: {
           diff_days: { $divide: [{ $subtract: [new Date(), new Date('$announceDate')] }, 1000 * 60 * 60 * 24] }
         }
-      },
-      {
-        $match: {
-          diff_days: { $gte: 30, $lt: 30 }
-        }
       }
     ]);
 
