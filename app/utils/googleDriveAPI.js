8d34693496f0c041c17cf0396039794a19a76465
Shahbaz Shoukat <shahbaz.shoukat@mesmerhq.com>
2021-06-19 13:49:11 +0500

    refresh token updated

--- app/utils/googleDriveAPI.js
+++ app/utils/googleDriveAPI.js
@@ -17,7 +17,7 @@ const CLIENT_ID = '985847113984-sdk1a97ecaf7o96tchgafnqh3m722939.apps.googleuser
 const CLIENT_SECRET = 'poFpMOOH15yxfo_UCnx18nKI';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
 
-const REFRESH_TOKEN = '1//048Op5suUjqTvCgYIARAAGAQSNwF-L9Irl_k-9NRruzJdMDjHO4DilIJyFLwP8sGzYta3kF2rTAxliZyJhbQmZlqoRboYx87SgKY';
+const REFRESH_TOKEN = '1//042prxT6Q85RvCgYIARAAGAQSNwF-L9IrmcGJHQdT_IeT_b1RT38SL-wruKyL9DzO50sSJ8ayXkETuhTO91KrP3E28y-E_Q9d6xk';
 
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
